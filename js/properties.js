7c9c75cee824acad082cf9fdb1f54d20488cd833
Cristian <schweizercristian@gmail.com>
2020-06-29 18:00:42 +0200

    First commit

---
+++ js/properties.js
@@ -0,0 +1,103 @@
+var data = [
+    {
+      "_id": "5ef9a90a2e42498f0bcea7f4",
+      "name": "villa NEW BUILD",
+      "house_size": 87,
+      "terrain_size": 827,
+      "price": "$403,091",
+      "bedrooms": 3,
+      "bathrooms": 3,
+      "energy_rating": "In process",
+      "year_built": 1980,
+      "orientation": "East",
+      "balcony": "11m2",
+      "distance_to_beach": "210 Mts",
+      "about": "Incididunt exercitation reprehenderit pariatur cillum elit aute duis quis elit cupidatat eiusmod. Ea ut minim occaecat esse id ex ut ullamco consequat aliquip. Lorem id anim commodo adipisicing excepteur cillum non do irure voluptate cupidatat ad ipsum.\r\n"
+    },
+    {
+      "_id": "5ef9a90ab21158643594c17d",
+      "name": "villa NEW BUILD",
+      "house_size": 61,
+      "terrain_size": 862,
+      "price": "$454,819",
+      "bedrooms": 3,
+      "bathrooms": 3,
+      "energy_rating": "In process",
+      "year_built": 1997,
+      "orientation": "North",
+      "balcony": "11m2",
+      "distance_to_beach": "500 Mts",
+      "about": "Ad eiusmod tempor et reprehenderit. Veniam velit aliquip in in pariatur in reprehenderit sunt ad pariatur laborum eiusmod. Ullamco ullamco fugiat ex tempor aute. Anim in id do mollit nulla amet quis. Cupidatat pariatur velit ea officia magna nulla. Consectetur cupidatat dolore ut est.\r\n"
+    },
+    {
+      "_id": "5ef9a90a4e3b6d6d8bb08169",
+      "name": "villa NEW BUILD",
+      "house_size": 118,
+      "terrain_size": 695,
+      "price": "$343,875",
+      "bedrooms": 1,
+      "bathrooms": 2,
+      "energy_rating": "In process",
+      "year_built": 1969,
+      "orientation": "East",
+      "balcony": "11m2",
+      "distance_to_beach": "210 Mts",
+      "about": "Nulla cillum magna aliqua esse ut dolor exercitation amet qui non esse est dolore. Fugiat proident Lorem tempor aute commodo elit nisi proident qui cupidatat. Cillum officia aliqua cillum cillum. Nulla commodo culpa ullamco Lorem dolore consectetur ex magna ad excepteur ut commodo adipisicing sit. Laboris et enim cupidatat anim occaecat fugiat veniam anim ex tempor sint cupidatat commodo. Enim eu occaecat deserunt ipsum consequat mollit et velit laborum nostrud dolore culpa non. Esse exercitation qui adipisicing minim ea labore voluptate esse nostrud amet voluptate.\r\n"
+    },
+    {
+      "_id": "5ef9a90a3454135531565aa4",
+      "name": "villa NEW BUILD",
+      "house_size": 87,
+      "terrain_size": 799,
+      "price": "$446,021",
+      "bedrooms": 1,
+      "bathrooms": 1,
+      "energy_rating": "In process",
+      "year_built": 1982,
+      "orientation": "South",
+      "balcony": "15m2",
+      "distance_to_beach": "210 Mts",
+      "about": "Tempor esse aute minim id quis minim et ex velit et qui nulla. Fugiat exercitation amet tempor sunt est proident amet incididunt eiusmod eiusmod. Sit exercitation tempor dolor occaecat proident nulla esse officia. Tempor ea fugiat ex esse consequat ut consectetur in. Aliquip labore esse nisi est ullamco minim duis occaecat occaecat deserunt aliquip Lorem.\r\n"
+    },
+    {
+      "_id": "5ef9a90ad8b9083dc8af4994",
+      "name": "villa NEW BUILD",
+      "house_size": 105,
+      "terrain_size": 970,
+      "price": "$445,029",
+      "bedrooms": 1,
+      "bathrooms": 2,
+      "energy_rating": "In process",
+      "year_built": 1961,
+      "orientation": "North",
+      "balcony": "7m2",
+      "distance_to_beach": "200 Mts",
+      "about": "Sunt elit exercitation minim voluptate mollit tempor sint et deserunt culpa dolor. In tempor qui qui deserunt exercitation excepteur enim pariatur Lorem irure ex excepteur. Non ad in pariatur eu incididunt commodo. Sint tempor culpa ipsum officia Lorem voluptate cupidatat aliqua dolore mollit esse. Consectetur minim deserunt aliquip aliquip nulla non ut occaecat deserunt nulla dolore.\r\n"
+    },
+    {
+      "_id": "5ef9a90a91355a0a41e9272e",
+      "name": "villa NEW BUILD",
+      "house_size": 67,
+      "terrain_size": 600,
+      "price": "$315,272",
+      "bedrooms": 2,
+      "bathrooms": 3,
+      "energy_rating": "In process",
+      "year_built": 2003,
+      "orientation": "North",
+      "balcony": "7m2",
+      "distance_to_beach": "500 Mts",
+      "about": "Ipsum adipisicing sunt tempor consectetur deserunt proident eiusmod. Eiusmod veniam eu quis tempor veniam veniam ea occaecat consectetur. Laborum nostrud ad ea eu officia sint adipisicing sint nostrud duis. Fugiat magna nostrud non magna do sunt tempor. Nisi quis cupidatat reprehenderit aliqua eiusmod id ad nisi dolore quis culpa esse veniam sint. Quis magna ipsum cillum fugiat exercitation adipisicing Lorem sint eiusmod id ullamco.\r\n"
+    }
+  ]
+
+  console.log(data);
+  /* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+}
