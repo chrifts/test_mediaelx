8e64dffabecb3d0eee862fdb30cd309fa85d1183
Cristian <schweizercristian@gmail.com>
2020-07-04 13:52:51 +0200

    update

--- js/home.js
+++ js/home.js
@@ -3,4 +3,8 @@ $("#btn_more_filters").on('click', function() {
     $("#advanced_serach_home").toggleClass('d-none');
     $(this).toggleClass("btn_filled_dark btn_outline_light");
     $(this).find("img").toggleAttr('src', '../Assests/advanced_white.svg', '../Assests/advanced.svg');
+    if(document.documentElement.clientWidth < 767) {
+      $("#container_post_header_home").toggleAttr('style', 'margin-top: 300px;','margin-top: 0px;')
+    }
+    
   })
